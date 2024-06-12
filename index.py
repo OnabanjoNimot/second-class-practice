e43406da373258299decee3fa3c9464e91d28b68
OnabanjoNimot <neemmert@gmail.com>
2024-06-12 13:21:00 +0000

    created python script that calculate grade

---
+++ index.py
@@ -0,0 +1,14 @@
+a ='learning git'
+print(a)
+
+score = 63
+if score > 75 and score <= 100:
+    print('A')
+elif score >= 60 and score < 75:
+    print('B')
+elif score >=50 and score < 60:
+    print('C')
+elif score >= 45 and score < 50:
+    print('D')
+else:
+    print('F')
